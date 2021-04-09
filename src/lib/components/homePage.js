c66cf82ab3becbb652bdcb2f1969968b0d6d4cdd
CarolAbcl <carolallende@gmail.com>
2021-04-08 22:01:20 -0400

    error required text

--- src/lib/components/homePage.js
+++ src/lib/components/homePage.js
@@ -8,9 +8,9 @@ export const homePage = () =>{
 }
 
 export const userLogin = () =>{
-  const loginForm = `<form class= 'loginForm'>
-                  <input type="text" id="signup-email"  placeholder="Email" required>
-                  <input type="password" id="signup-password" placeholder="Password" required>
+  const loginForm = `<form class= 'loginForm' id= 'loginForm'>
+                  <input type="text" id="signing-email"  placeholder="Email" required>
+                  <input type="password" id="signing-password" placeholder="Password" required>
                   <button type="submit">Log in</button>
                 </form>
                 `
@@ -18,7 +18,7 @@ export const userLogin = () =>{
 }
 
 export const userRegister = () =>{
- const registerForm = `<form class= 'registerForm'>
+ const registerForm = `<form class= 'registerForm' id= 'registerForm'>
                       <input type="text" id="userName"  placeholder="Nombre de Usuario" required>
                       <input type="text" id="signup-email"  placeholder="Correo electrónico" required>
                       <input type="password" id="signup-password" placeholder="Contraseña" required>
@@ -28,4 +28,6 @@ export const userRegister = () =>{
                       `
   return registerForm                  
 }
+
+//document.getElementById('login-button').addEventListener('click',userLogin());
 
