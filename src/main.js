b7984cad9460816695301d5bfd458c450a4afabd
karinmelissa <karin.escobar@usach.cl>
2021-04-14 12:42:27 -0400

    routes

--- src/main.js
+++ src/main.js
@@ -1,25 +1,30 @@
-import { startFunction } from './index.js';
+import { initRouter } from './route.js'
+/*import { startFunction } from './index.js';
 import { feedHome } from './lib/components/feed.js';
-import { close } from './lib/functions/auth.js';
+import { close } from './lib/functions/auth.js';*/
 
-firebase.auth().onAuthStateChanged((user) => {
+
+//  initRouter();
+/*firebase.auth().onAuthStateChanged((user) => {
   if (user) {
     document.getElementById('root').appendChild(feedHome());
     const logoutButton = document.getElementById('logout-button');
     logoutButton.addEventListener('click', close);
-    /* const createFeed = document.createRange().createContextualFragment(feedHome());
-    const logoutButton = createFeed.getElementById('logout-button');
-    document.getElementById('root').appendChild(createFeed);
-    logoutButton.addEventListener('click', console.log('hola'));
-    fs.collection("posts")
-      .get()
-      .then((snapshot) => {
-        setupPosts(snapshot.docs);
-        loginCheck(user);
-      }); */
   } else {
     document.getElementById('root').appendChild(startFunction());
-    /* setupPosts([]);
-    loginCheck(user); */
   }
-});
+});*/
+const init = () => {
+
+  //initFirebase();
+
+  //observer();
+  //templateCSS();
+  //templateHome();
+  initRouter();
+ // templateSignUp();
+  
+
+}
+
+window.addEventListener('load',init);
